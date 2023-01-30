4bfd88267f5e6070ac691fb92ada442439c12679
Junior Rios <wjuniorrios@gmail.com>
2023-01-30 11:46:16 -0300

    remove spacy test

--- src/spacy_predict.py
+++
@@ -1,8 +0,0 @@
-import spacy
-
-nlp = spacy.load("pt_core_news_lg", disable=["tagger", "attribute_ruler", "lemmatizer"])
-
-doc = nlp(
-    "A Dona Maria lavou a calçada com água e tomou um esporro do senhor Jamil que não gostou nada da situação!"
-)
-print([(w.text, w.pos_, w.ent_iob_, w.ent_type_) for w in doc])
